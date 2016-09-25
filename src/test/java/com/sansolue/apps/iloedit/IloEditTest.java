d17760538f86538453a89f4ec7ad2ee72f924558
Samith Sandanayake <samiths@zone24x7.com>
2016-09-25 16:52:32 +0530

    Application class

---
+++ src/test/java/com/sansolue/apps/iloedit/IloEditTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2016 by Samith Sandanayake.
+ *
+ * This software is licensed under Apache License 2
+ */
+
+package com.sansolue.apps.iloedit;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Samith Sandanayake
+ */
+public class IloEditTest {
+
+}
